ef820bb6c16b82dff02441cff6fd288b5c80261e
Tenchitrilogy <100139708+Tenchitrilogy@users.noreply.github.com>
2023-12-27 10:47:15 -0500

    3rd Edit
    Separated Skills/Projects/Certifications

--- Resume Website live 12-19-23/scripts.js
+++ Resume Website live 12-19-23/scripts.js
@@ -1,36 +1,59 @@
-// Fetch employment data dynamically (you'll need to fetch your data)
-const employmentData = [
-  {
-    title: 'Network Administrator',
-    company: 'Bloomfield Board of Education',
-    location: 'Bloomfield, NJ',
-    duration: '2009 - Present',
-    responsibilities: [
-      'Monitoring day-to-day network operations',
-      'Providing technical assistance',
-      // Add your responsibilities
-    ],
-    // Add other job details
-  },
-  // Add other job experiences
+document.addEventListener('DOMContentLoaded', function() {
+const timelineData = [
+    {
+        date: 'Oct 2009 - Present',
+        title: 'Network Administrator',
+        description: 'Proficient Network Administrator with solid background installing, configuring, and supporting network and internal systems. Verify operations and trace faults with skilled testing and troubleshooting. Thrive under pressure to keep systems optimized and user-friendly..'
+    },
+    {
+        date: 'Jan 2000 - Dec 2009',
+        title: 'L3 Travel Event Tech',
+        description: 'As a L3 Travel Event Tech at Rush Computer Rentals, I supervised major equipment rental projects across Las Vegas, Atlnta, and New York, delivering exceptional customer support, managing equipment specifications, installations, and providing technical expertise for network systems. Additionally, I oversaw staff training, problem-solving, and served as the primary liaison for client communication and project supervision.'
+    },
+    {
+        date: '1998 - 1999',
+        title: 'Laser Printer Tech',
+        description: 'I effectively managed customer service orders, providing comprehensive maintenance, repair, and technical support for Hewlett Packard and Lexmark printers, ensuring prompt resolution and reliable assistance.'
+    }
 ];
 
-// Populate employment timeline
-const employmentTimeline = document.getElementById('employment-timeline');
-employmentData.forEach(job => {
-  const jobDiv = document.createElement('div');
-  jobDiv.innerHTML = `
-    <h3>${job.title}</h3>
-    <p>${job.company} - ${job.location}</p>
-    <p>${job.duration}</p>
-    <ul>
-      ${job.responsibilities.map(responsibility => `<li>${responsibility}</li>`).join('')}
-    </ul>
-    <!-- Add more job details as needed -->
-  `;
-  employmentTimeline.appendChild(jobDiv);
-});
+    const timeline = document.getElementById('timeline');
 
-// Fetch and populate skills, projects, and certifications dynamically
-// Use JavaScript to create interactive elements and links to your GitHub and tryhackme.com badges
-// Add your code to populate these sections with your skills, projects, and certifications
+    timelineData.forEach((item, index) => {
+        let entry = document.createElement('div');
+        entry.classList.add('timeline-entry');
+        entry.setAttribute('id', 'entry-' + index);
+
+        let dot = document.createElement('div');
+        dot.classList.add('timeline-dot');
+
+        let date = document.createElement('div');
+        date.textContent = item.date;
+        date.classList.add('timeline-date');
+
+        let content = document.createElement('div');
+        content.classList.add('timeline-content');
+        content.setAttribute('id', 'content-' + index);
+
+        let title = document.createElement('h3');
+        title.textContent = item.title;
+
+        let description = document.createElement('p');
+        description.textContent = item.description;
+        description.style.display = 'none'; // Initially hide the description
+
+        // Click event to toggle the description visibility
+        entry.addEventListener('click', function() {
+            description.style.display = description.style.display === 'none' ? 'block' : 'none';
+        });
+
+        content.appendChild(title);
+        content.appendChild(description);
+
+        entry.appendChild(dot);
+        entry.appendChild(date);
+        entry.appendChild(content);
+
+        timeline.appendChild(entry);
+    });
+});
